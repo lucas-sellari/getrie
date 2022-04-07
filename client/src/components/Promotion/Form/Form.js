f572c2641fa449496f3a0c1beeaa6bea27adbbb2
Lucas Sellari <lucasmiguel448@gmail.com>
2022-04-07 07:41:41 -0300

    implementada a função de dar update no promotionForm

--- client/src/components/Promotion/Form/Form.js
+++ client/src/components/Promotion/Form/Form.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { SwapSpinner } from "react-spinners-kit";
 import axios from "axios";
 import "./Form.css";
 
@@ -10,9 +11,23 @@ const initialValue = {
   url: "",
 };
 
-const PromotionForm = () => {
-  const [values, setValues] = useState(initialValue);
+const PromotionForm = ({ id }) => {
+  const [values, setValues] = useState(id ? null : initialValue);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id) {
+      //edição...
+      axios
+        .get(`http://localhost:3333/promotions/${id}`)
+        .then((response) => {
+          setValues(response.data);
+        })
+        .catch((error) => {
+          console.log(error.toJSON());
+        });
+    }
+  }, []);
 
   const onChange = (event) => {
     const { name, value } = event.target;
@@ -23,9 +38,12 @@ const PromotionForm = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
+    const method = id ? "put" : "post";
+    const url = id
+      ? `http://localhost:3333/promotions/${id}`
+      : "http://localhost:3333/promotions";
 
-    axios
-      .post("http://localhost:3333/promotions", values)
+    axios[method](url, values)
       .then((response) => {
         navigate("/");
       })
@@ -37,38 +55,70 @@ const PromotionForm = () => {
   return (
     <div>
       <h1>PromoShow</h1>
-      <h2>Nova Promoção</h2>
+      <h2>{id ? "Editar" : "Nova"} Promoção</h2>
 
-      <form onSubmit={onSubmit}>
-        <div className="promotion-form__group">
-          <label htmlFor="title">Título</label>
-          <input id="title" name="title" type="text" onChange={onChange} />
+      {!values ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "120px",
+          }}
+        >
+          <SwapSpinner />
         </div>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <div className="promotion-form__group">
+            <label htmlFor="title">Título</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              onChange={onChange}
+              value={values.title}
+            />
+          </div>
 
-        <div className="promotion-form__group">
-          <label htmlFor="url">Link</label>
-          <input id="url" name="url" type="text" onChange={onChange} />
-        </div>
+          <div className="promotion-form__group">
+            <label htmlFor="url">Link</label>
+            <input
+              id="url"
+              name="url"
+              type="text"
+              onChange={onChange}
+              value={values.url}
+            />
+          </div>
 
-        <div className="promotion-form__group">
-          <label htmlFor="imageUrl">Imagem (URL)</label>
-          <input
-            id="imageUrl"
-            name="imageUrl"
-            type="text"
-            onChange={onChange}
-          />
-        </div>
+          <div className="promotion-form__group">
+            <label htmlFor="imageUrl">Imagem (URL)</label>
+            <input
+              id="imageUrl"
+              name="imageUrl"
+              type="text"
+              onChange={onChange}
+              value={values.imageUrl}
+            />
+          </div>
 
-        <div className="promotion-form__group">
-          <label htmlFor="price">Preço</label>
-          <input id="price" name="price" type="number" step="0.01" onChange={onChange} />
-        </div>
+          <div className="promotion-form__group">
+            <label htmlFor="price">Preço</label>
+            <input
+              id="price"
+              name="price"
+              type="number"
+              step="0.01"
+              onChange={onChange}
+              value={values.price}
+            />
+          </div>
 
-        <div>
-          <button type="submit">Salvar</button>
-        </div>
-      </form>
+          <div>
+            <button type="submit">Salvar</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
