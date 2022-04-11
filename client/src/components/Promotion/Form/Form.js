cb50987337917169912bb13385615a3122b2758e
Lucas Sellari <lucasmiguel448@gmail.com>
2022-04-11 19:04:40 -0300

    integração do formulário com o Formik e utilização do yup para a criação do validation scheme

--- client/src/components/Promotion/Form/Form.js
+++ client/src/components/Promotion/Form/Form.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { SwapSpinner } from "react-spinners-kit";
 import useApi from "components/utils/useApi.js";
+import Field from "components/Form/Field/Field.js";
+import { Formik, Form } from "formik";
+import schema from "./Schema.js";
 import "./Form.css";
 
 const initialValue = {
@@ -12,15 +15,11 @@ const initialValue = {
 };
 
 const PromotionForm = ({ id }) => {
-  const [values, setValues] = useState(id ? null : initialValue);
   const navigate = useNavigate();
 
-  const [load] = useApi({
+  const [load, loadInfo] = useApi({
     url: `/promotions/${id}`,
     method: "get",
-    onCompleted: (response) => {
-      setValues(response.data);
-    },
   });
 
   const [update, updateInfo] = useApi({
@@ -41,19 +40,13 @@ const PromotionForm = ({ id }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
-  const onChange = (event) => {
-    const { name, value } = event.target;
-    //console.log({ name, value });
-
-    setValues({ ...values, [name]: value });
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = (formValues) => {
     update({
-      data: values,
+      data: formValues,
     });
   };
+
+  const values = id ? loadInfo.data : initialValue;
 
   return (
     <div>
@@ -71,68 +64,47 @@ const PromotionForm = ({ id }) => {
           <SwapSpinner />
         </div>
       ) : (
-        <form onSubmit={onSubmit}>
-          <div className="promotion-form__group">
-            <label htmlFor="title">Título</label>
-            <input
-              id="title"
-              name="title"
-              type="text"
-              onChange={onChange}
-              value={values.title}
-            />
-          </div>
+        <Formik
+          initialValues={values}
+          onSubmit={onSubmit}
+          validationSchema={schema}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className="promotion-form__group">
+                <Field name="title" type="text" label="Título" />
+              </div>
 
-          <div className="promotion-form__group">
-            <label htmlFor="url">Link</label>
-            <input
-              id="url"
-              name="url"
-              type="text"
-              onChange={onChange}
-              value={values.url}
-            />
-          </div>
+              <div className="promotion-form__group">
+                <Field name="url" type="text" label="Link" />
+              </div>
 
-          <div className="promotion-form__group">
-            <label htmlFor="imageUrl">Imagem (URL)</label>
-            <input
-              id="imageUrl"
-              name="imageUrl"
-              type="text"
-              onChange={onChange}
-              value={values.imageUrl}
-            />
-          </div>
+              <div className="promotion-form__group">
+                <Field name="imageUrl" type="text" label="Imagem (URL)" />
+              </div>
 
-          <div className="promotion-form__group">
-            <label htmlFor="price">Preço</label>
-            <input
-              id="price"
-              name="price"
-              type="number"
-              step="0.01"
-              onChange={onChange}
-              value={values.price}
-            />
-          </div>
+              <div className="promotion-form__group">
+                <Field name="price" type="number" step="0.01" label="Preço" />
+              </div>
 
-          <div>
-            <button type="submit">Salvar</button>
-          </div>
-          {updateInfo.loading && (
-            <span
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "20px",
-                color: "#888",
-              }}
-            >
-              <h1>Salvando dados... 🤗</h1>
-            </span>
+              <div>
+                <button type="submit">Salvar</button>
+              </div>
+              {updateInfo.loading && (
+                <span
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "20px",
+                    color: "#888",
+                  }}
+                >
+                  <h1>Salvando dados... 🤗</h1>
+                </span>
+              )}
+            </Form>
           )}
-        </form>
+        </Formik>
       )}
     </div>
   );
